bbe5c533ca433465519f6031be69e1c1ee8d7e1d
SrinivasPothala <65867936+SrinivasPothala@users.noreply.github.com>
2022-08-14 20:08:25 -0500

    Added a initial docs for blackjack

---
+++ src/main/java/com/example/blackjackDemo/BlackjackDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.blackjackDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackjackDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackjackDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blackjackDemo/BlackjackDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blackjackDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackjackDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
